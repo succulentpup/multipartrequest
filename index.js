2f4990bfb6fdb0c24d081b55db99245abb496cbb
CivilBot <civilbots@gmail.com>
2018-08-03 00:52:40 +0530

    multipart/form-data tried

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const req = require('request');
+const fs = require('fs-extra');
 const app = express();
 
 app.use((request, response, next) => {
@@ -13,15 +14,48 @@ app.use((request, response, next) => {
 });
 
 app.get('/', (request, response) => {
-    getRequest(request, response);
+    //getRequest(request, response);
+    getMultipartFormData(request, response);
 });
+
+const getMultipartFormData = ( request, response ) => {
+    const formData = {
+        // Pass a simple key-value pair
+        my_field: 'my_value',
+        // Pass data via Buffers
+        my_buffer: Buffer.from([1, 2, 3]),
+        // Pass data via Streams
+        my_file: fs.createReadStream(__dirname + '/attachment.pdf'),
+        // Pass multiple values /w an Array
+        attachments: [
+            fs.createReadStream(__dirname + '/attachment.pdf'),
+            fs.createReadStream(__dirname + '/attachment.pdf')
+        ]
+    };
+    req.post(
+        {
+            url: 'http://locahost:3000',
+            "Content-Type": "multipart/form-data",
+            formData: formData
+        },(err, result) => {
+            if (err) {
+                console.log("Error occured: ", err);
+                response.send({Error: err});
+            } else{
+                response.send({ result : res });
+            }
+        }
+    )
+};
 
 const getRequest = ( request, response ) => {
     req.get('https://static.h-bid.com/gdpr/cmp.stub.js', (err, res) => {
-       if (err)  response.send({status : "200 OK"});
-       response.send({
-           result : res
-       });
+       if (err) {
+           console.log("Error occured: ", err);
+           response.send({Error: err});
+       } else{
+           response.send({ result : res });
+       }
     });
 };
 
